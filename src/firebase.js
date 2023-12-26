54a90da994498474482cfe0993fd90d24ed8e2e3
Aakrati jain <103743579+aakrati-123@users.noreply.github.com>
2023-12-26 20:29:59 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const config = {
-  apiKey: "AIzaSyAvjUyYv5P57WowCOqGoDJTQpurwgSRm5U",
-  authDomain: "aakrati-linkedin-clone.firebaseapp.com",
-  projectId: "aakrati-linkedin-clone",
-  storageBucket: "aakrati-linkedin-clone.appspot.com",
-  messagingSenderId: "795862302466",
-  appId: "1:795862302466:web:7304f08a766a6f9c228c1b",
-
+  apiKey: "AIzaSyAiVH4B8dffhBOULAY8oEneUzZXbWKSxWY",
+  authDomain: "aakrati-linkedin-clone-b2311.firebaseapp.com",
+  projectId: "aakrati-linkedin-clone-b2311",
+  storageBucket: "aakrati-linkedin-clone-b2311.appspot.com",
+  messagingSenderId: "991615798387",
+  appId: "1:991615798387:web:ea432df9fa9ca2c4d66e06",
+  measurementId: "G-PZVWK1MZSD"
 };
 
 const app = initializeApp(config);
